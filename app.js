3cb6a7e8eae85710901649fd44de455bae0f8fd1
Manuel-Lauriola <manuelauriol@gmail.com>
2025-09-18 18:16:59 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// importo express
+const express = require(`express`)
+
+//utilizzo express che ho appena importato
+app = express()
+
+//definisco la porta in cui il server si metterà in ascolto
+const port = 3000
+
+//definisco la rotta base
+app.get("/", (req, res) => {
+  res.send(`rotta base del mio server`)
+})
+
+//dico al server di rimanere in ascolto sulla porta definita
+app.listen(port, () => {
+  console.log(`server in ascolto sulla ${port}`)
+})
+
